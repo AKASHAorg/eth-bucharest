9bb9f4cce159ffa999a65d7636721e1c24f00465
Vali Cotea <quinine35@gmail.com>
2024-03-21 12:56:37 +0200

    chore(): try apollo hook

--- apps/example-app/src/components/simple-antenna/index.tsx
+++ apps/example-app/src/components/simple-antenna/index.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import Stack from '@akashaorg/design-system-core/lib/components/Stack';
 import BeamResolver from './beam-resolver';
-import { useGetBeams } from './use-get-beams';
 import useIntersectionObserver from '@akashaorg/design-system-core/lib/utils/intersection-observer';
+// import { useGetBeams } from './use-get-beams';
+import { useGetBeamsQuery } from '@akashaorg/ui-awf-hooks/lib/generated/apollo';
 
 export type SimpleAntennaProps = {};
 
 const SimpleAntenna: React.FC<SimpleAntennaProps> = () => {
-  const { beams, fetchInitialData, fetchNextBatch } = useGetBeams();
-  React.useEffect(() => {
-    fetchInitialData();
-  }, []);
+  // const { beams, fetchInitialData, fetchNextBatch } = useGetBeams();
+  // React.useEffect(() => {
+  //   fetchInitialData();
+  // }, []);
+
+  const { data, loading, error, fetchMore } = useGetBeamsQuery({
+    variables: { last: 10 },
+  });
+
+  const beams = React.useMemo(() => {
+    return data.akashaBeamIndex?.edges;
+  }, [data]);
+
+  const handleFetchMore = () => {
+    fetchMore({
+      variables: {
+        before: data.akashaBeamIndex?.pageInfo?.endCursor,
+        last: 10,
+      },
+      updateQuery(previousResult, { fetchMoreResult }) {
+        const previousEdges = previousResult.akashaBeamIndex.edges;
+        const newEdges = fetchMoreResult.akashaBeamIndex?.edges;
+        const newPageInfo = fetchMoreResult.akashaBeamIndex?.pageInfo;
+        return {
+          pageInfo: newPageInfo,
+          edges: [...previousEdges, ...newEdges],
+        };
+      },
+    });
+  };
 
   const loadMoreRef = React.createRef<HTMLDivElement>();
 
   useIntersectionObserver({
     target: loadMoreRef,
-    onIntersect: fetchNextBatch,
+    onIntersect: handleFetchMore,
     threshold: 0,
   });
 
